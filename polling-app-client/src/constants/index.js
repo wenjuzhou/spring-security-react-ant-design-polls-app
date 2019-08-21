32e5a8d0c5b0d4d01a4c1c2a2bb15fa65fc29e14
Winton <yskj@Wintons-MacBook-Pro.local>
2019-08-21 20:27:53 +0800

    modified keyfiles

--- polling-app-client/src/constants/index.js
+++ polling-app-client/src/constants/index.js
@@ -1,4 +1,9 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+
+let API_URL = 'http://localhost:8080/api';
+if (process.env.APISERVER_URL) {
+    API_URL = `${process.env.APISERVER_URL}/api`;
+}
+export const API_BASE_URL = API_URL;
 export const ACCESS_TOKEN = 'accessToken';
 
 export const POLL_LIST_SIZE = 30;
